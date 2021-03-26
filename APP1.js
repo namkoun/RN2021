140c9571349940ccda8f8662b2700ec77a8975fc
namkoun <skarud0328@gmail.com>
2021-03-26 15:53:02 +0900

    init 0326

---
+++ APP1.js
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import type {Node} from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+ 
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+ 
+ class App extends React.Component{
+   constructor() {
+     super();
+     this.state = {
+       year: 2021,
+       leapYear: true,
+       name: 'Sang Jun Park',
+       topics: ['React', 'React Native', 'JavaScript'],
+       info: {
+         paperback: true,
+         length: '335 pages',
+         type: 'programming',
+       }
+     }
+   }
+   
+   render() {
+     let leapyear = <Text>this is not a leapyear!</Text>
+     if (this.state.leapYear) {
+       leapyear = <Text>this is a leapyear</Text>
+     }
+     return(
+       <View>
+         <Text>
+         the year is : { this.state.year }
+ 
+         </Text>
+         <Text>length: {this.state.info.length} </Text>
+         <Text>type: {this.state.info.type} </Text>     
+         <Text>topics: {this.state.topics[1]} </Text>  
+         {leapyear}  
+       </View>
+     )
+   }
+ }
+ 
+ export default App;
--- App.js
+++ App.js
@@ -27,36 +27,20 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 class App extends React.Component{
-  constructor() {
-    super();
-    this.state = {
-      year: 2021,
-      leapYear: true,
-      name: 'Sang Jun Park',
-      topics: ['React', 'React Native', 'JavaScript'],
-      info: {
-        paperback: true,
-        length: '335 pages',
-        type: 'programming',
-      }
-    }
+  render(){
+    let book="React Native in Action!"
+    return (
+      <BookDisplay book ={ book } />
+    )
   }
   
+    
+}
+class BookDisplay extends React.Component{
   render() {
-    let leapyear = <Text>this is not a leapyear!</Text>
-    if (this.state.leapYear) {
-      leapyear = <Text>this is a leapyear</Text>
-    }
-    return(
+    return (
       <View>
-        <Text>
-        the year is : { this.state.year }
-
-        </Text>
-        <Text>length: {this.state.info.length} </Text>
-        <Text>type: {this.state.info.type} </Text>     
-        <Text>topics: {this.state.topics[1]} </Text>  
-        {leapyear}  
+        <Text>{ this.props.book }</Text>
       </View>
     )
   }
