4f9cd8acfc9eee8f5d552d9e9963449736949426
namkoun <skarud0328@gmail.com>
2021-04-09 15:50:33 +0900

    Init0409

--- app/App.js
+++ app/App.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react'
 import { View, ScrollView, StyleSheet } from 'react-native'
+import Button from './Button'
 import Heading from './Heading'
 import Input from './Input'
+
+let todoIndex = 0
+
 class App extends Component{
     constructor(){
         super()
@@ -10,10 +14,26 @@ class App extends Component{
             todos:[],
             type:'All'
         }
+        this.submitTodo=this.submitTodo.bind(this)
     }
     inputChange(inputValue){
         console.log('input: ',inputValue)
         this.setState({inputValue})
+    }
+    submitTodo (){
+        if (this.state.inputValue.match(/^\s*$/)){
+            return //비워있으면 아무것도 반환하지 않고
+        }
+        const todo = { // 비어있지 않으면 todo 변수를 생성해서 다음의 객체를 할당한다
+            title:this.state.inputValue,
+            todoIndex,
+            complete:false
+        }
+        todoIndex++ //인덱스 증가
+        const todos=[...this.state.todos, todo] //새로운 todo를 기존 배열에 추가
+        this.setState({todos, inputValue:''},()=>{ //todo의 state를 지정해 갱신된 배열의
+            console.log('State: ',this.state)       // inputvalue를 빈 문자열로 재지정
+        })
     }
     render() {
         const {inputValue}=this.state
@@ -25,6 +45,7 @@ class App extends Component{
                 <Input 
                 inputValue={inputValue}
                 inputChange={(text) => this.inputChange(text)} />
+                <Button submitTodo={this.submitTodo}></Button>
                 </ScrollView>
             </View>
         )
