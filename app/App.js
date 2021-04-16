81e9ce7c1286c72d526e54ac8762f720bd1f5c2a
namkoun <skarud0328@gmail.com>
2021-04-16 15:33:31 +0900

    INIT(4-16)3장끝

--- app/App.js
+++ app/App.js
@@ -4,6 +4,7 @@ import Button from './Button'
 import Heading from './Heading'
 import Input from './Input'
 import TodoList from './TodoList'
+import TabBar from './TabBar'
 
 let todoIndex = 0
 
@@ -20,6 +21,7 @@ class App extends Component{
         this.submitTodo=this.submitTodo.bind(this)
         this.toggleComplete = this.toggleComplete.bind(this)
         this.deleteTodo = this.deleteTodo.bind(this)
+        this.setType = this.setType.bind(this)
     }
     inputChange(inputValue){
         console.log('input: ',inputValue)
@@ -41,7 +43,9 @@ class App extends Component{
         })
     }
     
-
+ setType (type){
+    this.setState({type})
+ }
     
     
     deleteTodo (todoIndex){
@@ -50,18 +54,18 @@ class App extends Component{
         this.setState({todos})
     }
     toggleComplete (todoIndex){
-        let todos = this.state.todos
+        let todos = this.state.todos 
 
-todos.forEach((todo) =>{
+   todos.forEach((todo) =>{
     if(todo.todoIndex ===todoIndex){
         todo.complete = !todo.complete
     }
-})    
-this.setState({todos})
-}
+   })    
+ this.setState({todos})
+ }
 
     render() {
-        const {inputValue, todos}=this.state
+        const {inputValue, todos, type}=this.state
         return(
             <View style={styles.container}>
                 <ScrollView keyboardShouldPersistTaps='always' 
@@ -71,11 +75,13 @@ this.setState({todos})
                 inputValue={inputValue}
                 inputChange={(text) => this.inputChange(text)} />
                 <TodoList 
+                type={type}
                 toggleComplete = {this.toggleComplete}
                 deleteTodo={this.deleteTodo}
                 todos={todos} />
                 <Button submitTodo={this.submitTodo} />
                 </ScrollView>
+                <TabBar type={type} setType={this.setType} />
             </View>
         )
     }
